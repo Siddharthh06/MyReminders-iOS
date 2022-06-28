23bd3986cb4df5da2da86c09565e91b5eb07334a
Siddharth Gupta <72133884+Siddharthh06@users.noreply.github.com>
2022-06-29 01:04:28 +0530

    Add files via upload

---
+++ MyReminders_coredata/MyReminders/ReminderItem/ReminderItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ReminderItem+CoreDataClass.swift
+//  MyReminders
+//
+//  Created by Siddharth Gupta on 20/06/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ReminderItem)
+public class ReminderItem: NSManagedObject {
+
+}
